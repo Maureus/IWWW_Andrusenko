8155923361ea34737c5ffacbdcf3904c086abfe5
Maureus <62718184+Maureus@users.noreply.github.com>
2020-11-13 22:44:42 +0100

    Update reset-request.inc.php

--- test_andrew/includes/reset-request.inc.php
+++ test_andrew/includes/reset-request.inc.php
@@ -47,7 +47,7 @@ if (isset($_POST["reset-request-submit"])) {
       $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
       $mail->Username   = 'sswepss@gmail.com';                     // SMTP username
-      $mail->Password   = 'qhncpwwezmminmgm';                               // SMTP password
+      $mail->Password   = '';                               // SMTP password
       $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
       $mail->Port       = 587;                                      // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
